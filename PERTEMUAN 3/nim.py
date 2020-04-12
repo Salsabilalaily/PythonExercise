9b99cc6655af960bf00bff40551e0df9d804bdf9
Salsabila Laily Rahma <61041484+Salsabilalaily@users.noreply.github.com>
2020-04-13 02:02:10 +0700

    Create nim.py

---
+++ PERTEMUAN 3/nim.py
@@ -0,0 +1,11 @@
+nim=list(input().split('/'))
+index=input()
+
+if index=='ANGKATAN':
+    print(nim[0])
+elif index=='NIU':
+    print(nim[1])
+elif index=='FAKULTAS':
+    print(nim[2])
+elif index=='NIF':
+    print(nim[3])
