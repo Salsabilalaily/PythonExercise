79782e42582c5ba57fb0277af434d9a6ee7c8001
Salsabila Laily Rahma <61041484+Salsabilalaily@users.noreply.github.com>
2020-04-13 01:20:21 +0700

    Create crayon.py

---
+++ PERTEMUAN 1/crayon.py
@@ -0,0 +1,15 @@
+acara=input()
+singkatan=input()
+cek_sama=0
+jumlah_sama=0
+
+for i in range(len(acara)):
+  for j in range(cek_samaa,len(singkatan)):
+    if singkatan[j]==acara[i]
+      jumlah_sama=jumlah_sama+1
+      cek_sama=cek_sama+1
+if jumlah_sama==len(singkatan):
+  print('BISA')
+else:
+  print('TIDAK BISA')
+  
